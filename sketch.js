0a3b50c211e5d876760c0f165691b0b81dc2ddb0
MJ1119-c <rarachu1234@gmail.com>
2024-10-31 22:18:55 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,37 +1,53 @@
-let angle = 0
+let font; // 載入字型文字
+let points = []; // 轉成點狀文字
+let r = 8; // 增加上下幅度
+let angle = 0; // 三角函數內的角度
+
+function preload() { // 在執行setup()前，必須先做此函數執行，主要先載入字型
+ font = loadFont("fonts/Rowdies-Bold.ttf"); // 載入在fonts資料夾內的Rowdies-Bold.ttf字型
+}
+
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  background(0)
-  rectMode(CENTER)  //畫方形以中心點為座標點
-  noFill() //方形不填入顏色
-  angleMode(DEGREES) //設定角度單位為0~360
-  //frameRate(100) //設定每秒進入draw()函數的次數
+ createCanvas(windowWidth, windowHeight, WEBGL);
+ points = font.textToPoints("TKUET", -300, 80, 200, {
+ sampleFactor: 0.5
+}); // 轉成文字圖檔，放在(0, 0)位置，圖形大小為200，sampleFactor為點數距離大小
+ angleMode(DEGREES); // 宣告角度使用0~360
 }
-function draw() {
-  background(0); //背景黑
-  for(let y=0;y<height;y=y+120){
-    for(let x=0;x<width;x = x+120){
-      push() //重新設定格式(包含原點(0,0)位置，線條顏色..)，要設調原點(0,0)的位置在(x,y)上
-        translate(x,y) //把原點由視窗的左上角移到視窗的中心點
-        stroke(255) //線條顏色
-        //===============1
-        //rotate(angle) //把物件選轉10度的角度，以原點(0,0)作為旋轉基準點
-        //rect(0,0,600,600,100) //畫一個方形，在視窗中間，寬與高都為600
-        //angle = sin(frameCount) *100 //-100~100
-        //===============10
-        for(let i=0;i<10;i=i+1) {
-          // let r = random(50,255) //抽一個亂數值，介於50(含)與255(不含)間
-          // let g = random(50,255) //抽一個亂數值，介於50(含)與255(不含)間
-          // let b = random(50,255) //抽一個亂數值，介於50(含)與255(不含)間
-          let r = map(sin(frameCount),-1,1,50,255)
-          let b = map(cos(frameCount/2),-1,1,50,70)
-          let g = map(sin(frameCount/4),-1,1,50,70)
-          stroke(r,g,b)
-          rotate(angle) //把物件選轉10度的角度，以原點(0,0)作為旋轉基準點
-          rect(0,0,100-i*3,100-i*3 ,20) //畫一個方形，在視窗中間，寬與高都為600
-          angle = sin(frameCount) *30 //-100~100
-        }
-      pop() //取消所有設定格式，原點(0,0)回到視窗的左上角
-    }
-  }
+
+//===================================================
+
+function draw() { // 畫圖
+  // 背景圖案
+ background("#edf6f9");
+ translate(-width / 2, -height / 2);
+ noFill();
+ strokeWeight(2);
+ let rect_width = 50;
+ let bc_w = 50;
+ let sc_w = 25;
+ 
+  let scaleFactor = map(mouseY, 0, height, 1, 2); // 調整放大倍率
+  rectMode(CENTER);
+  for (let j = 0; j < 30; j++) {
+  for (let x = 0; x < width; x += rect_width) {
+    ellipse(x, 25 + 50 * j, bc_w * scaleFactor);
+    rect(x, 25 + 50 * j, rect_width * scaleFactor);
+    ellipse(25 + x + rect_width, 50 + 50 * j, sc_w * scaleFactor);
+   }
+ }
+
+ // 前景動態效果
+translate(width / 2, height / 2);
+  rotateX(angle % 360);
+  for (let i = 0; i < points.length - 1; i++) {
+   fill("#eef4ed");
+   ellipse(points[i].x + r * sin(angle + i * 25), points[i].y + r * sin(angle + i * 25), 10);
+   strokeWeight(3);
+   stroke("#efd3d7");
+   line(points[i].x + r * sin(angle + i * 25), points[i].y + r * sin(angle + i * 25), points[i + 1].x, points[i + 1].y);
+ }
+
+  angle += 10; // 每畫圖一次就要調整角度+10
 }
+
